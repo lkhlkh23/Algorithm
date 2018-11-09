1c8b98652764edbf6bca356b0f2a1ced84f12927
lkhlkh23 <lkhlkh09@gmail.com>
2018-11-09 11:44:46 +0900

    2018.11.09

--- Desktop/CodeSquad/Algorithm/BaekJoon/Q1094/Main.java
+++ Desktop/CodeSquad/Algorithm/BaekJoon/Q1094/Main.java
@@ -8,7 +8,7 @@ public class Main {
    /*
        문제 : 막대기
        url : https://www.acmicpc.net/problem/1094
-       재풀이 : X
+       재풀이 : O
     */
 
     public static void main(String[] args) throws NumberFormatException, IOException {
@@ -16,29 +16,31 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int size = 64;
-        Stack<Integer> stack = new Stack<>();
-        stack.add(size);
-        int sum = 0;
-        while((size + sum) != n) {
-            size = stack.pop() / 2;
-            if(size + sum >= n) {
-                stack.add(size);
-            } else {
-                stack.add(size);
-                stack.add(size);
-                sum += size;
-            }
-        }
 
-        System.out.println(stack.size());
-        System.out.println(Integer.bitCount(n));
+        System.out.println(getResultByStack(n));
+        System.out.println(geResultByBitCount(n));
 
         bw.close();
     }
 
-    public static int geResult(int num) {
+    public static int geResultByBitCount(int num) {
         return Integer.bitCount(num);
+    }
+
+    public static int getResultByStack(int num) {
+        int size = 64;
+        Stack<Integer> stack = new Stack<>();
+        stack.add(size);
+        int sum = 0;
+        while((size + sum) != num) {
+            size = stack.pop() / 2;
+            if(size + sum < num) {
+                stack.add(size);
+                sum += size;
+            }
+            stack.add(size);
+        }
+        return stack.size();
     }
 
 }
