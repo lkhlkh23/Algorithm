c38a17df484b4633b960cf292679c929f5834cbf
lkhlkh23 <lkhlkh09@gmail.com>
2018-12-09 23:06:27 +0900

    2018.12.09

---
+++ Desktop/CodeSquad/Algorithm/BaekJoon/Q15894/Main.java
@@ -0,0 +1,17 @@
+package Q15894;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+   /*
+       문제 : 수학은 체육과목입니다.
+       url : https://www.acmicpc.net/problem/15894
+       재풀이 : X
+    */
+
+    public static void main(String[] args) {
+        System.out.println(new BigInteger(new Scanner(System.in).nextLine()).multiply(new BigInteger("4")) + "\n");
+    }
+
+}
---
+++ Desktop/CodeSquad/Algorithm/Programmers/Q0069/Solution.java
@@ -0,0 +1,26 @@
+package Q0069;
+
+public class Solution {
+    public int solution(int[] numbers, int target) {
+        return DFS(numbers, target, 0, 0);
+    }
+
+    public int DFS(int[] numbers, int target, int index, int num) {
+        if(index == numbers.length) {
+            return num == target ? 1 : 0;
+        } else {
+            return DFS(numbers, target, index + 1, num + numbers[index])
+                    + DFS(numbers, target, index + 1, num - numbers[index]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[20];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1;
+        }
+        int target = 3;
+
+        System.out.println(new Solution().solution(arr, target));
+    }
+}
