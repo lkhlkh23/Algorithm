85bf6bc992f3dad1e44e7fa90d8f3045859d09bf
lkhlkh23 <lkhlkh09@gmail.com>
2018-12-16 17:18:39 +0900

    2018.12.16

---
+++ Desktop/CodeSquad/Algorithm/BaekJoon/Q11279/Main.java
@@ -0,0 +1,32 @@
+package Q11279;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Main {
+   /*
+       문제 : 최대힙
+       url : https://www.acmicpc.net/problem/11279
+       재풀이 : O
+    */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num > 0) pq.add(num);
+            else sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.close();
+    }
+}
