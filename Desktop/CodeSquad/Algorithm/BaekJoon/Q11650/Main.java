fe53b615c9810dd93590437135df1843c00acfa4
lkhlkh23 <lkhlkh09@gmail.com>
2018-12-27 19:43:19 +0900

    2018.12.27

--- Desktop/CodeSquad/Algorithm/BaekJoon/Q11650/Main.java
+++ Desktop/CodeSquad/Algorithm/BaekJoon/Q11650/Main.java
@@ -1,14 +1,38 @@
 package Q11650;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
    /*
-        문제 : 좌표정렬하기
-        url : https://www.acmicpc.net/problem/11650
-        재풀이 : X
+      문제 : 좌표 정렬하기
+      url : https://www.acmicpc.net/problem/11650
+      재풀이 : O
    */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        List<Point> points = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            points.add(new Main().new Point(Integer.parseInt(input[0]), Integer.parseInt(input[1])));
+        }
+
+        Collections.sort(points, (o1, o2) -> o1.x - o2.x > 0 || (o1.x - o2.x == 0 && o1.y - o2.y > 0) ? 1 : -1);
+
+        for(Point point : points)
+            sb.append(point.x).append(" ").append(point.y).append("\n");
+
+        bw.write(sb.toString());
+        bw.close();
+    }
 
     public class Point {
         public int x;
@@ -18,25 +42,5 @@ public class Main {
             this.x = x;
             this.y = y;
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine());
-        Point[] arr = new Point[n];
-        for(int i = 0; i < n; i++) {
-            String[] input = br.readLine().split(" ");
-            arr[i] = new Main().new Point(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
-        }
-        Arrays.sort(arr, (o1, o2) -> (o1.x - o2.x > 0 || (o1.x -o2.x > 0 && o1.y - o2.y > 0) ? 1 : -1));
-
-        for(int i = 0; i < arr.length; i++)
-            sb.append(String.format("%d %d", arr[i].x, arr[i].y)).append(System.lineSeparator());
-
-        bw.write(sb.toString());
-        bw.close();
     }
 }
