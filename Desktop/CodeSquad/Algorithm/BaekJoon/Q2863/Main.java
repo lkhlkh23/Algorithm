759df3fe99aa4e7c3dd8e8fb5ba52a87bed13441
lkhlkh23 <lkhlkh09@gmail.com>
2019-01-01 17:00:04 +0900

    2019.01.01

--- Desktop/CodeSquad/Algorithm/BaekJoon/Q2863/Main.java
+++ Desktop/CodeSquad/Algorithm/BaekJoon/Q2863/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         String[] input = br.readLine().split(" ");
         int a = Integer.parseInt(input[0]);
         int b = Integer.parseInt(input[1]);
@@ -20,20 +21,22 @@ public class Main {
         int c = Integer.parseInt(input[0]);
         int d = Integer.parseInt(input[1]);
 
-        double[] arr = new double[5];
-        arr[1] = ((double)c / d) + ((double)a / b);
-        arr[2] = ((double)d / b) + ((double)d / a);
-        arr[3] = ((double)b / a) + ((double)d / c);
-        arr[4] = ((double)a / c) + ((double)b / d);
+        double[] arr = {
+                ((double)a / c) + ((double)b / d),
+                ((double)c / d) + ((double)a / b),
+                ((double)d / b) + ((double)c / a),
+                ((double)b / a) + ((double)d / c)
+        };
 
         double max = 0;
         int index = 0;
-        for(int i = 1; i < arr.length; i++) {
+        for(int i = 0; i < arr.length; i++) {
             if(max < arr[i]) {
                 max = arr[i];
                 index = i;
             }
         }
+
         bw.write(index + "\n");
         bw.close();
     }
