9bad104f711bf10afba209d4c3fd6e99fbc2cacd
lkhlkh23 <lkhlkh09@gmail.com>
2019-02-06 17:02:43 +0900

    2019.02.06

--- Desktop/CodeSquad/Algorithm/Programmers/Q43164/Solution.java
+++ Desktop/CodeSquad/Algorithm/Programmers/Q43164/Solution.java
@@ -7,47 +7,62 @@ import java.util.stream.Collectors;
 class Solution {
     public String[] solution(String[][] tickets) {
         /* 출발지 초기화 */
-        List<Node> list = new ArrayList<>();
+        List<Node> nodeList = new ArrayList<>();
         for (int i = 0; i < tickets.length; i++) {
-            list.add(new Node(tickets[i][0], tickets[i][1]));
+            nodeList.add(new Node(tickets[i][0], tickets[i][1]));
         }
 
-        //Node departure = list.get(0);
-        //Node destination = list.get(list.size() - 1);
+        Collections.sort(nodeList, ((o1, o2) -> o1.departure.compareTo((o2.departure)) != 0
+                ? o1.departure.compareTo((o2.departure)) : o1.destination.compareTo(o2.destination)));
 
-        Collections.sort(list, (o1, o2) -> o1.departure.compareTo(o2.departure));
-        System.out.println(list.toString());
+        String[] answer = new String[nodeList.size() + 1];
+        for (Node node : nodeList) {
+            boolean[] visited = new boolean[nodeList.size()];
+            List<Node> result = new ArrayList<>();
+            dfs(nodeList, node, result, visited);
+            if(result.size() == nodeList.size()) {
+                System.out.println(result.toString());
+                break;
+            }
+        }
 
-        List<String> answers = new ArrayList<>();
-        String[] answer = new String[answers.size()];
-        Stack<Node> stack = new Stack<>();
-        stack.add(list.get(0));
-        answers.add(list.get(0).departure);
+        return answer;
+    }
 
-        while(!stack.isEmpty()) {
-            Node pop = stack.pop();
-            for(Node other : list) {
-                if(!other.isVisited() && pop.isAdjacent(other)) {
-                    stack.add(other);
-                    other.visit();
-                    System.out.println(other.toString());
+    public Node dfs(List<Node> tickets, Node departure, List<Node> result, boolean[] visited) {
+        if(index == arr.length - 1) {
+            return departure;
+        } else {
+            for (Node ticket : tickets) {
+                if(!ticket.isVisited() && departure.isAdjacent(ticket)) {
+                    ticket.visit();
+                    ticket.addNode(ticket, departure.list);
+                    return dfs(tickets, ticket, cnt);
                 }
             }
         }
-        //answers.add(destination.destination);
-        System.out.println(answers.toString()+"~~~~~~");
-
-        return answer;
+        return null;
     }
 
     public class Node {
         public String departure;
         public String destination;
         public boolean visited;
+        List<String> list = new ArrayList<>();
 
         public Node(String departure, String destination) {
             this.departure = departure;
             this.destination = destination;
+        }
+
+
+        public boolean isAdjacent(Node other) {
+            return this.destination.equals(other.departure);
+        }
+
+        public void addNode(Node node) {
+            this.list.add(node.departure);
+            this.list.add(node.destination);
         }
 
         public void visit() {
@@ -58,12 +73,8 @@ class Solution {
             return this.visited;
         }
 
-        public boolean isAdjacent(Node other) {
-            return this.destination.equals(other.departure);
-        }
-
-        public boolean isEqualTicket(Node other) {
-            return this.destination.equals(other.destination) && this.departure.equals(other.departure);
+        public int obtainListSize() {
+            return this.list.size();
         }
 
         @Override
@@ -73,6 +84,7 @@ class Solution {
                     ", destination='" + destination + '\'' +
                     '}';
         }
+
     }
 
     public static void main(String[] args) {
@@ -86,6 +98,13 @@ class Solution {
 
         String[][] tickets2 = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}};
         answers = solution.solution(tickets2);
+        for(String str : answers) {
+            System.out.print(str + " ");
+        }
+        System.out.println();
+
+        String[][] tickets3 = {{"A", "D"}, {"A", "B"}, {"A", "E"}};
+        answers = solution.solution(tickets3);
         for(String str : answers) {
             System.out.print(str + " ");
         }
