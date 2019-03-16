788440d0a4d0718b417983c70de4ce50809e6670
lkhlkh23 <lkhlkh09@gmail.com>
2019-03-16 17:25:36 +0900

    Concurrency Control

--- Desktop/CodeSquad/Algorithm/concurrent/Test_1.java
+++ Desktop/CodeSquad/Algorithm/concurrent/Test_1.java
@@ -2,25 +2,64 @@ package concurrent;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 public class Test_1 {
 
-    private static int count = 1;
-
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(2);
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        Counter counter = new Test_1().new Counter();
+        IntStream.range(0, 10000)
+                .forEach(i -> executor.submit(counter::plus));
 
-        IntStream.range(0, 1000)
-                .forEach(i -> executor.submit(new Runnable() {
-                    @Override
-                    public void run() {
-                        count += 1;
-                    }
-                })
-        );
+        executor.shutdown();
+        System.out.println(counter.getCount());  // 973
 
-        System.out.println(count);  // 973
+        ExecutorService executor2 = Executors.newFixedThreadPool(10);
+        AtomicCounter atomicCounter = new Test_1().new AtomicCounter();
+        IntStream.range(0, 10000)
+                .forEach(i -> executor2.submit(atomicCounter::plus));
 
+        executor2.shutdown();
+
+        System.out.println(atomicCounter.getCount());  // 973
+
+    }
+
+    public class Counter {
+
+        private int count = 0;
+
+        public Counter() {
+
+        }
+
+        public void plus() {
+            synchronized (this) {
+                count++;
+            }
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
+    public class AtomicCounter {
+
+        private AtomicInteger count = new AtomicInteger(0);
+
+        public AtomicCounter() {
+
+        }
+
+        public void plus() {
+            count.addAndGet(1);
+        }
+
+        public int getCount() {
+            return count.get();
+        }
     }
 }
